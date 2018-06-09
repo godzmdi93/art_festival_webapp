98cbc37527ce4ad7e11c607fde9841580829709d
hedgiejo <31581239+hedgiejo@users.noreply.github.com>
2018-06-09 13:21:33 -0400

    Add location
    Add google maps api to show location of state college at the end of site.

---
+++ ArtFest/main.js
@@ -0,0 +1,11 @@
+
+// Initialize Firebase (ADD YOUR OWN DATA)
+var config = {
+  apiKey: "AIzaSyDFre2AWWqsq_Rs2eAIToS9g2qgxsMJeS0",
+  authDomain: "hello-ce547.firebaseapp.com",
+  databaseURL: "https://hello-ce547.firebaseio.com",
+  projectId: "hello-ce547",
+  storageBucket: "",
+  messagingSenderId: "478942910626"
+};
+firebase.initializeApp(config);
