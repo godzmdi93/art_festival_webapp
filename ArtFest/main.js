509d8b55bac19e86dc220ae5f492b121e272a046
hedgiejo <31581239+hedgiejo@users.noreply.github.com>
2018-06-09 15:25:48 -0400

    Firebase connection established
    Contact form connected to firebase, checkbox and referred are not conencted yet.

--- ArtFest/main.js
+++ ArtFest/main.js
@@ -1,11 +1,55 @@
-
-// Initialize Firebase (ADD YOUR OWN DATA)
+// Initialize Firebase
 var config = {
-  apiKey: "AIzaSyDFre2AWWqsq_Rs2eAIToS9g2qgxsMJeS0",
-  authDomain: "hello-ce547.firebaseapp.com",
-  databaseURL: "https://hello-ce547.firebaseio.com",
-  projectId: "hello-ce547",
-  storageBucket: "",
-  messagingSenderId: "478942910626"
+  apiKey: "AIzaSyA9VpnncQtXeiASQ_kuIHSWvv0c2dEteKk",
+  authDomain: "contactform-1be84.firebaseapp.com",
+  databaseURL: "https://contactform-1be84.firebaseio.com",
+  projectId: "contactform-1be84",
+  storageBucket: "contactform-1be84.appspot.com",
+  messagingSenderId: "861580409827"
 };
 firebase.initializeApp(config);
+
+// Reference messages collection
+var messagesRef = firebase.database().ref('messages');
+
+// Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e){
+  e.preventDefault();
+
+  // Get values
+  var name = getInputVal('name');
+  var email = getInputVal('email');
+  var message = getInputVal('message');
+
+  // Save message
+  saveMessage(name, email, message);
+
+  // Show alert
+  document.querySelector('.alert').style.display = 'block';
+
+  // Hide alert after 3 seconds
+  setTimeout(function(){
+    document.querySelector('.alert').style.display = 'none';
+  },3000);
+
+  // Clear form
+  document.getElementById('contactForm').reset();
+}
+
+// Function to get get form values
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(name, email, message){
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    email:email,
+    message:message
+  });
+}
