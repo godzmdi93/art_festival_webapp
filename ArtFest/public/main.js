d077690693046cc0e66edf89848c2716574be86d
hedgiejo <31581239+hedgiejo@users.noreply.github.com>
2018-06-11 21:47:18 -0400

    Add Google sign in
    Add google sign in feature, basic sign-in form. Need to add confirmation page for the users.

--- ArtFest/public/main.js
+++ ArtFest/public/main.js
@@ -1,3 +1,7 @@
+var provider = new firebase.auth.GoogleAuthProvider();
+var user;
+var selectedFile;
+
 // Initialize Firebase
 var config = {
   apiKey: "AIzaSyA9VpnncQtXeiASQ_kuIHSWvv0c2dEteKk",
@@ -53,9 +57,29 @@ function saveMessage(name, email, message){
     message:message
   });
 }
-var selectedFile;
 
 
+// sign in
+
+function signIn() {
+	firebase.auth().signInWithPopup(provider).then(function(result) {
+	  // This gives you a Google Access Token. You can use it to access the Google API.
+	  var token = result.credential.accessToken;
+	  // The signed-in user info.
+	  user = result.user;
+	}).catch(function(error) {
+	  // Handle Errors here.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+	  // The email of the user's account used.
+	  var email = error.email;
+	  // The firebase.auth.AuthCredential type that was used.
+	  var credential = error.credential;
+	});
+}
+
+
+// image upload
 	document.getElementById("upload").addEventListener('change', handleFileSelect, false);
 
 
